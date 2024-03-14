5ca8ee3abf28c2e931f0aa9f7dae99b8c626f056
thevtn <73998182+thevtn@users.noreply.github.com>
2024-03-14 18:03:56 +0300

    Add files via upload

--- t2me.py
+++ t2me.py
@@ -7,79 +7,87 @@ from requests.exceptions import JSONDecodeError, ConnectionError
 init()
 
 delay = input("Включить задержку? (Y/n): ").lower()
-trafficType = input("Введите цифру типа трафика (1-ГБ или 2-минуты): ")
+trafficType = input("Введите цифру типа трафика (1-ГБ, 2-минуты, 3-SMS): ")
 traffic_types = {"1": "data", "2": "voice", "3": "sms"}
 trafficType = traffic_types.get(trafficType, trafficType)
-
+headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 13; Redmi Note 10S) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36'}
+def InputErrHandler():
+    os.system('clear')
+    print(Fore.YELLOW + "Произошла ошибка при обработке данных.\nПожалуйста, проверьте правильность введенных данных и попробуйте снова.")
+    raise SystemExit
+def ErrorHandler():
+    print(Fore.YELLOW + "Ошибка парсинга: потерян доступ к серверу.")
+    raise SystemExit
 volume = int(input("Введите кол-во ГБ/минут: "))
 mincost = input("Минималка? (Y/n): ").lower()
 yes = {'yes','y', 'ye', ''}
 no = {'no','n'}
 limit = input("Введите кол-во отображаемых продавцов: ")
-def ErrorHandler():
-    print(Fore.YELLOW + "Ошибка парсинга: потерян доступ к серверу.")
-    raise SystemExit
 if mincost in yes:
-    headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 13; Redmi Note 10S) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36'}
+    
     cost_url = f"https://tele2.ru/api/exchange/lots/stats/volumes?trafficType={trafficType}"
     try:
-       response = requests.get(cost_url, headers=headers)
-       data = response.json()
+        response = requests.get(cost_url, headers=headers)
+        data = response.json()
     except:
-       ErrorHandler()
-    def get_cost(volume):
-        for item in data['data']:
-            if item['volume'] == volume:
-                cost = item["minCost"]
-                return cost
-        return None
-    cost = get_cost(volume)
+        ErrorHandler()
+    try:      
+        def get_cost(volume):
+            for item in data['data']:
+                if item['volume'] == volume:
+                    cost = item["minCost"]
+                    return cost
+            return None
+        cost = get_cost(volume)
+    except:
+            InputErrHandler()    
 elif mincost in no:
-    cost = input("Введите цену лота: ")
+        cost = input("Введите цену лота: ")
         
 while True:
     url = f"https://tele2.ru/api/exchange/lots?trafficType={trafficType}&volume={volume}&cost={cost}&limit={limit}"
-    headers = {"user-agent": "Mozilla/5.0 (Linux; Android 13; Redmi Note 10S) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36"}
+    
 
     try:
        if delay in yes:
           time.sleep(5)
        else:
-          time.sleep(0)
+          time.sleep(1)
        response = requests.get(url, headers=headers)
        dump = response.json()
     except:
-       ErrorHandler()
+        ErrorHandler()
 
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('clear')
     
     trafficType = " ГБ" if trafficType == "data" else " минут(ы)" if trafficType == "voice" else " SMS"
-     
-    print(f"{Fore.GREEN}Информация о лоте:")
-    print(f"{Fore.GREEN}Лот: {volume}{trafficType}")
-    print(f"{Fore.GREEN}Цена: {str(cost).replace('.0', '')} ₽")
-    print(f"{Fore.WHITE}{'-' * 35}{Fore.RESET}")
-    print(f"{Fore.YELLOW}{'Имя':<20}{'Эмодзи'}{Fore.RESET}")
-    print(f"{Fore.WHITE}{'-' * 35}{Fore.RESET}")
-    if "data" in dump:
-        try:
-            for item in dump["data"]:
-                seller = item.get("seller", {})
-                name = seller.get("name")
-                emojis = seller.get("emojis")
-                trafficType = item.get("trafficType")
-                my = item.get("my")
+    try:
+        print(f"{Fore.GREEN}Информация о лоте:")
+        print(f"{Fore.GREEN}Лот: {volume}{trafficType}")
+        print(f"{Fore.GREEN}Цена: {str(cost).replace('.0', '')} ₽")
+        print(f"{Fore.WHITE}{'-' * 39}{Fore.RESET}")
+        print(f"{Fore.YELLOW}{'Имя':<20}{'Эмодзи'}{Fore.RESET}")
+        print(f"{Fore.WHITE}{'-' * 39}{Fore.RESET}")    
+        if "data" in dump:
+                for item in dump["data"]:
+                    seller = item.get("seller", {})
+                    name = seller.get("name")
+                    emojis = seller.get("emojis")
+                    trafficType = item.get("trafficType")
+                    my = item.get("my")
+                    emoji_dict = {"devil": "\U0001F608", "cool": "\U0001F60E", "cat": "\U0001F431", "zipped": "\U0001F910", "scream": "\U0001F631", "rich": "\U0001F911", "tongue": "\U0001F61B", "bomb": "\U0001F4A3", "": "  "}
 
-                name = "Анонимный продавец" if name is None else name
-                if my is False:
-                    name = Fore.GREEN + str(name)
-                    emojis = Fore.GREEN + str(emojis) + Fore.RESET
-                else:
-                    name = Fore.RED + str(name)
-                    emojis = Fore.RED + str(emojis) + Fore.RESET
+                    for i in range(len(emojis)):
+                        emojis[i] = emoji_dict[emojis[i]]
 
-                print(f"{Fore.WHITE}{name:<25}{emojis}{Fore.RESET}")
-        except TypeError as e:
-            os.system('clear')
-            print(Fore.YELLOW + "Произошла ошибка при обработке данных.\nПожалуйста, проверьте правильность введенных данных и попробуйте снова.")
-            raise SystemExit
+                    name = "Анонимный продавец" if name is None else name
+                    if my is False:
+                        name = Fore.GREEN + str(name)
+                        emojis = Fore.GREEN + str(emojis) + Fore.RESET
+                    else:
+                        name = Fore.RED + str(name)
+                        emojis = Fore.RED + str(emojis) + Fore.RESET
+
+                    print(f"{Fore.WHITE}{name:<25}{emojis}{Fore.RESET}")
+    except:
+            InputErrHandler()
